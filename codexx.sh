0576cc605c0b1cec39dd0ce34fbf5a6b474ab43b
Shivang Verma <shivang260279@gmail.com>
2022-06-07 14:48:18 +0530

    Update codexx.sh

--- codexx.sh
+++ codexx.sh
@@ -15,6 +15,10 @@ echo "Enter the Domain you want to enumerate : "
 echo ""
 read Domain
 echo ""
+echo "Do you want to use nuclei on the resultant list ? [ 1 - Yes/0 - No ] "
+echo
+read nuke
+echo
 echo "Enter 1 for custom settings of each tool (Enter 0 for default settings)"
 read settings
 
@@ -25,9 +29,9 @@ then
 	echo ""
 	echo "(!!! Dont enter domain and output flags !!!)"
 	echo ""
-	declare -a tools=( Findomain subfinder sublist3r amass )
+	declare -a tools=( Findomain subfinder sublist3r amass nuclei )
 	declare -a flags
-	for i in {0..3}
+	for i in {0..4}
 	do
 		echo "Flag for " ${tools[i]} " : "
 		read flag
@@ -36,24 +40,32 @@ then
 	echo ""
 
 	findomain ${flags[0]} -t $Domain --output
-	subfinder ${flags[1]} -d $Domain >sub2.txt
-	sublist3r ${flags[2]} -d $Domain -o sub3.txt
-	amass enum ${flags[3]} -d $Domain -o sub4.txt
+	subfinder ${flags[1]} -d $Domain >codexxtmp1.txt
+	sublist3r ${flags[2]} -d $Domain -o codexxtmp2.txt
+	amass enum ${flags[3]} -d $Domain -o codexxtmp3.txt
 
 else
 	findomain -t $Domain --output
-	subfinder -d $Domain >sub2.txt
-	sublist3r -d $Domain -o sub3.txt
-	amass enum -d $Domain -o sub4.txt
+	subfinder -d $Domain >codexxtmp1.txt
+	sublist3r -d $Domain -o codexxtmp2.txt
+	amass enum -d $Domain -o codexxtmp3.txt
 fi
 
-cat $Domain.txt sub2.txt sub3.txt sub4.txt >sub.txt
-rm $Domain.txt sub2.txt sub3.txt sub4.txt
-sort -u sub.txt > $Domain.txt
-rm sub.txt
+cat $Domain.txt codexxtmp1.txt codexxtmp2.txt codexxtmp3.txt >codexxtmp4.txt
+rm $Domain.txt codexxtmp1.txt codexxtmp2.txt codexxtmp3.txt
+sort -u codexxtmp4.txt > $Domain.txt
+rm codexxtmp4.txt
 httprobe < $Domain.txt > alive.$Domain.txt
-
+if [ $nuke == 1 ];then
+	if [ $settings == 1 ];then
+		nuclei ${flags[4]} -l alive.$Domain.txt -o nuclei.$Domain.txt
+	else
+		nuclei -l alive.$Domain.txt -o nuclei.$Domain.txt
+	fi
+fi
 echo "Results stored in $Domain.txt and alive.$Domain.txt in the current directory."
 echo ""
+echo "Nuclei results stored in nuclei.$Domain.txt file"
+echo 
 echo "Enjoy !!!"
 
